2db512664368bff9655262dde550de294bf4ff9f
PelamingTheExploderBust <cravenpelaming@gmail.com>
2026-01-09 12:29:17 +0000

    yes

---
+++ PaymentProcessor.java
@@ -0,0 +1,16 @@
+public class PaymentProcessor {
+
+    public void processPayment(double amount, String currency) {
+        System.out.println("Processing payment of " + amount + " " + currency);
+    }
+
+    public void processPayment(double amount, String currency, String method) {
+        System.out.println("Processing payment of " + amount + " " + currency + " using " + method);
+    }
+
+    public static void main(String[] args) {
+        PaymentProcessor process = new PaymentProcessor();
+        process.processPayment(120, "Dollah");
+        process.processPayment(500, "Petot", "GCash");
+    }
+}
